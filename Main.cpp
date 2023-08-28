7238e2f8dad34949fdb789e5fd65e72acffa450f
Nizar Zariouh <nizarzariouh05@gmail.com>
2023-08-28 05:46:28 -0400

    commit 3d rendering

---
+++ Main.cpp
@@ -0,0 +1,310 @@
+#include <iostream>
+#include <vector>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "Shader.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+#include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+
+const float SCREEN_WIDTH = 800.0f;
+const float SCREEN_HEIGHT = 600.0f;
+
+float sizeX = 1.0f;
+float sizeY = 1.0f;
+float sizeZ = 1.0f;
+
+std::vector<std::string> shapes;
+
+unsigned int input;
+
+GLuint VBO, VAO;
+
+float positionY = 0.0f;
+float positionX = 0.0f;
+float positionZ = 0.0f;
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+
+}
+
+void setDefaultSettings() {
+		sizeX = 1.0f;
+		sizeY = 1.0f;
+		sizeZ = 1.0f;
+		positionX = 0.0f;
+		positionY = 0.0f;
+		positionZ = 0.0f;
+}
+
+void drawTriangle() {
+
+}
+
+void drawRectangle() {
+	// rectangle
+	float vertices[] = {
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+
+
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+}
+
+int main() {
+	// set up glfw and version opengl
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+	// window creation
+	GLFWwindow* window = glfwCreateWindow((int)SCREEN_WIDTH, (int)SCREEN_HEIGHT, "3D Renderer", NULL, NULL);
+	if (window == nullptr) {
+		std::cout << "FAILED TO CREATE WINDOW" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// load glad before opengl
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize glad" << std::endl;
+		return -1;
+	}
+	
+	// shader initialization
+	Shader shader("default.vert", "default.frag");
+
+	// opengl viewport
+	glViewport(0, 0, (int)SCREEN_WIDTH, (int)SCREEN_HEIGHT);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	drawRectangle();
+	drawTriangle();
+	glEnable(GL_DEPTH_TEST);
+
+
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// ST
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3*sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// texture generation
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load("assets/dirt_block.png", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		std::cout << "Texture loaded" <<  std::endl;
+	}
+	else {
+		std::cout << "Failed to load the texture" << std::endl;
+	
+	}
+	stbi_image_free(data);
+
+
+	shader.use();
+	shader.setInt("texture1", 0);
+	
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 330");
+
+	int shapesI = 0;
+
+
+	// render loop
+	while (!glfwWindowShouldClose(window)) {
+		// check input
+		processInput(window);
+		// rendering
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+
+		if (shapes.empty()) {
+			setDefaultSettings();
+		}
+
+		shader.use();
+		glm::mat4 view = glm::mat4(1.0f);
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		glm::mat4 projection;
+		projection = glm::ortho(-2.0f, +2.0f, -1.5f, +1.5f, 0.1f, 100.0f);
+
+		int viewLoc = glGetUniformLocation(shader.getID(), "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+		int projectionLoc = glGetUniformLocation(shader.getID(), "projection");
+		glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::rotate(model, -glm::radians(55.f), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::translate(model, glm::vec3(positionX, positionY, positionZ));
+		model = glm::scale(model, glm::vec3(0.25f, 0.25f, 0.25f));
+		int modelLoc = glGetUniformLocation(shader.getID(), "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+		glBindVertexArray(VAO);
+
+		for (int i = 0; i < shapes.size(); i++) {
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+		ImGui::Begin("Settings");
+		ImGui::Text("Position:");
+		ImGui::SliderFloat("X Axis", &positionX, -3.0f, 3.0f);
+		ImGui::SliderFloat("Y Axis", &positionY, -3.0f, 3.0f);
+		ImGui::SliderFloat("Z Axis", &positionZ, -3.0f, 3.0f);
+		ImGui::Text("Scale: ");
+		ImGui::SliderFloat("X", &sizeX, 0.f, 3.0f);
+		ImGui::SliderFloat("Y", &sizeY, 0.f, 3.0f);
+		ImGui::SliderFloat("Z", &sizeZ, 0.f, 3.0f);
+		ImGui::Text("Rotation:");
+		ImGui::End();
+
+		ImGui::Begin("Shapes");
+		if (ImGui::Button("Rectangle")) {
+			std::string rectangle = "rectangle" + char(shapesI);
+			shapes.push_back(rectangle);
+			shapesI++;
+		}
+		if (ImGui::Button("Triangle")) {
+			std::cout << "Triangle!" << std::endl;
+			shapes.push_back("triangle");
+			shapesI++;
+		}
+		if (ImGui::Button("Delete Previous")) {
+			if (!shapes.empty()) {
+				shapes.pop_back();
+				shapesI--;
+			}
+		}
+		if (ImGui::Button("Clear")) {
+			while (!shapes.empty()) {
+				shapes.pop_back();
+				shapesI = 0;
+			}
+		}
+		ImGui::End();
+
+		ImGui::Begin("Scene");
+		for (int i = 0; i < shapes.size(); i++) {
+			ImGui::Text(shapes[i].c_str());
+		}
+		ImGui::End();
+
+		shader.setFloat("sizeX", sizeX);
+		shader.setFloat("sizeY", sizeY);
+		shader.setFloat("sizeZ", sizeZ);
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		// check buffers and events
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	shader.remove();
+
+	glfwTerminate();
+	return 0;
+}
